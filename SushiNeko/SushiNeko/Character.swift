7504b877a9ec5283e9083de5fa1cec57c32ed274
RaymondDashWu <raymond31670@hotmail.com>
2019-11-03 22:34:10 -0800

    [ADD] Sushi Neko part 3

---
+++ SushiNeko/SushiNeko/Character.swift
@@ -0,0 +1,28 @@
+import SpriteKit
+
+class Character: SKSpriteNode {
+
+    /* Character side */
+    var side: Side = .left {
+        didSet {
+            if side == .left {
+                xScale = 1
+                position.x = 70
+            } else {
+                /* An easy way to flip an asset horizontally is to invert the X-axis scale */
+                xScale = -1
+                position.x = 252
+            }
+        }
+    }
+
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ SushiNeko/SushiNeko/GameScene.swift
@@ -0,0 +1,28 @@
+import SpriteKit
+
+/* Tracking enum for use with character and sushi side */
+enum Side {
+    case left, right, none
+}
+
+class GameScene: SKScene {
+    /* Game objects */
+    var sushiBasePiece: SushiPiece!
+    /* Cat Character */
+    var character: Character!
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        
+        /* Connect game objects */
+        sushiBasePiece = childNode(withName: "sushiBasePiece") as! SushiPiece
+//        character = childNode(withName: "character") as! Character
+
+        
+        
+        sushiBasePiece.connectChopsticks()
+
+
+    }
+    
+}
