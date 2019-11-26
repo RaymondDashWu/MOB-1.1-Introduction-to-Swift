f8dc9f4d74db4b1f0a0158792bd0ca644855034e
RaymondDashWu <raymond31670@hotmail.com>
2019-11-25 22:05:56 -0800

    [FIX/ADD] SugarTahoe classifier + added Magic8ball tutorial

--- ClassifyingImagesWithVisionAndCoreML/Vision+ML Example/ImageClassificationViewController.swift
+++ ClassifyingImagesWithVisionAndCoreML/Vision+ML Example/ImageClassificationViewController.swift
@@ -28,7 +28,7 @@ class ImageClassificationViewController: UIViewController {
              To use a different Core ML classifier model, add it to the project
              and replace `MobileNet` with that model's generated Swift class.
              */
-            let model = try VNCoreMLModel(for: makeschool_dog_identifier().model)
+            let model = try VNCoreMLModel(for: SugarOrTahoe().model)
             
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processClassifications(for: request, error: error)
@@ -77,13 +77,12 @@ class ImageClassificationViewController: UIViewController {
                 self.classificationLabel.text = "Nothing recognized."
             } else {
                 // Display top classifications ranked by confidence in the UI.
-                let topClassifications = classifications.prefix(2)
+                let topClassifications = classifications.prefix(1)
                 let descriptions = topClassifications.map { classification in
                     // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
-                   return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                   self.classificationLabel.text = "Hmm 🤔... I am " + String(format: "%.2f", (classification.confidence * 100)) + "% confident this is" + "\n" + String( classification.identifier)
                 }
-                self.classificationLabel.text = "Hmm 🤔... I think this is: \n" + descriptions.joined(separator: "\n")
-            }	
+            }
         }
     }
     
---
+++ MagicEightBall/MagicEightBall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagicEightBall
+//
+//  Created by Raymond MBP on 11/23/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
